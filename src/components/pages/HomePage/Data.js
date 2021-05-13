c5ae5eb3c2d27e52492a7ee1e70a21899b0391c0
Aline Alves <48930961+afsalves@users.noreply.github.com>
2021-05-13 10:19:51 -0300

    Adding home session

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,56 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'EQUIPE ESPECIALIZADA',
+    headline: 'Selecionamos os melhores prestadores de serviço',
+    description:
+      'Selecionamos os melhores profissionais do mercado para você ficar tranquilo. Seu carro estará em boas mãos!',
+    buttonLabel: 'COMECE A USAR',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Engenheiro'
+  };
+  
+  export const homeObjTwo = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '100% SEGURO',
+    headline: 'Seu carro protegido em todo momento e todo lugar',
+    description:
+      'Nós te cobrimos não importa onde você esteja. Temos parceiros em localidades do país todo para garantir que você tenha o serviço que precisa a qualquer momento',
+    buttonLabel: 'Saiba Mais',
+    imgStart: '',
+    img: 'images/svg-5.svg',
+    alt: 'Suporte Tecnico'
+  };
+  
+  export const homeObjThree = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'SERVIÇO FACILITADO',
+    headline: 'Contratação super rápida pelo site ou pelo App',
+    description:
+      "Cadastre seus dados e meio de pagamento em menos de 10 minutos. É tudo que você precisa para solicitar seu serviço, rápido e simples.",
+    buttonLabel: 'Solicitar Serviço',
+    imgStart: 'start',
+    img: 'images/svg-7.svg',
+    alt: 'Serviço Automotivo'
+  };
+  
+  export const homeObjFour = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'VARIEDADE DE SERVIÇOS',
+    headline: 'Todos os serviços que seu carro precisa em um só lugar',
+    description:
+      'Nós selecionamos os melhores estabelecimentos para cuidar do seu carro, e você tem tudo facilitado em  um único lugar.',
+    buttonLabel: 'Cadastre-se Agora',
+    imgStart: 'start',
+    img: 'images/svg-8.svg',
+    alt: 'Projeto de Serviço'
+  };
+  
